b5412e76f4a3ad0100fefac7b499987553315a62
Arne Sahlberg <arne.sahlberg@gmail.com>
2020-02-13 09:59:12 +0100

    Added functionality to parse several arguments and flags.
    Changed name of "checkarg" to "checkflag".

--- SimpleArgParse.py
+++ SimpleArgParse.py
@@ -16,6 +16,40 @@ def parsearg(arg, standardvalue=None, type=str):
   return type(sys.argv[index+1])
 
 
+# return argument input where several arguments are valid, 
+# i.e. -i or --input cound mean the same thing. then call
+# parseargs(["-i", "--input"]
+def parseargs(args, standardvalue=None, type=str):
+  for arg in args:
+    parsed = parsearg(arg, standardvalue=standardvalue, type=type)
+    if parsed is not None:
+      return parsed
+  return None # if none of the arguments were found
+
+
 # check if argument is given
-def checkarg(arg):
+def checkflag(arg):
   return arg in sys.argv
+
+
+# check any of a list of arguments are given
+def checkflags(args):
+  for arg in args:
+    check = checkflag(arg)
+    if check:
+      return True
+  return False
+  
+
+
+# main mehtod for testing
+if __name__ == "__main__":
+  a = parseargs(["-i", '--input'])
+  if a is not None:
+    print("Input", a, "given.")
+
+  b = checkflags(["-f", "--flag"])
+  if b:
+    print("Found flag -f or --flag.")
+
+
